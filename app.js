fbb0bb5d748fb0ad7dfe03d80491dfcf0a5d63ef
Ali Haider <alihaidercheema08@gmail.com>
2025-01-09 09:56:32 +0500

    Adding url to CORS

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 
 // Configure CORS to allow requests from the frontend
 const corsOptions = {
-  origin: ['http://localhost:3000', 'null','http://127.0.0.1:3000'], // Add other allowed origins (e.g., 'null' for local file access)
+  origin: ['http://localhost:3000', 'null','http://127.0.0.1:3000','https://api.khcrf.org'], // Add other allowed origins (e.g., 'null' for local file access)
   methods: 'GET,POST,DELETE,UPDATE,PUT',
   allowedHeaders: 'Content-Type, Authorization',
 };
